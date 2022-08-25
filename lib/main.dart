4c43b38c5071f1b1c2bea238747607f11f361e85
Ayush Verma <ayush7607verma@gmail.com>
2022-08-25 23:36:39 +0530

    day 3 completed yeah!!

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-// ctrl + ,  setting
-//  ctrl + shift + y terminal
-// ctrl + shift + p command palette
-// ctrl + + SIZE INC
-//  run f5
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1_vs/home_page.dart';
+import 'package:flutter_application_1_vs/screens/home_page.dart';
+import 'package:flutter_application_1_vs/screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,8 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    int day=10;
+    int day = 10;
     // num take double + int
     // string s="";
     // bool
@@ -25,8 +20,37 @@ class MyApp extends StatelessWidget {
     //  var day=5;automatic detect
 
     return MaterialApp(
-      home: homepage(),
-    );
+        // home: homepage(),
+
+        themeMode: ThemeMode.light,
+        // here if light given then goes to below else dark choose goes to darktheme :
+
+        theme: ThemeData(primarySwatch: Colors.pink),
+        // primary swatch set of colors which chang whole theme wrt color chosse smartly
+
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+          // these are specification of dark theme
+        ),
+        // initialRoute: "/home",
+        routes: {
+          "/": (context) => const loginpage(),
+          "/home": (context) => homepage(),
+          "/login": (context) => loginpage()
+        });
     // ui ka kaam
   }
 }
+
+// bring vegetables({@required bool thaila ,int rupees=100})
+// {
+//   // inside curly int rupees means if rupee not pass from main
+//   //  them it take default value 100
+//   // if written @required then compulosry pass
+// }
+
+// ctrl + ,  setting
+//  ctrl + shift + y terminal
+// ctrl + shift + p command palette
+// ctrl + + SIZE INC
+//  run f5
