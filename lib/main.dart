11dd048135be14da3671ad39dc6292b99c5efb43
Ayush Verma <ayush7607verma@gmail.com>
2022-08-25 18:06:24 +0530

    day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,32 @@
+// ctrl + ,  setting
+//  ctrl + shift + y terminal
+// ctrl + shift + p command palette
+// ctrl + + SIZE INC
+//  run f5
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1_vs/home_page.dart';
 
-
-void main(){
-
+void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  // used for passing data
 
   @override
   Widget build(BuildContext context) {
-    
+
+    int day=10;
+    // num take double + int
+    // string s="";
+    // bool
+    // const
+    //  var day=5;automatic detect
+
     return MaterialApp(
-      home: Material(
-        child : Center(
-      child: Container (
-         child: Text("Lauda Welcome"),
-      ),
-        ), 
-    ),
-     );
+      home: homepage(),
+    );
     // ui ka kaam
   }
 }
