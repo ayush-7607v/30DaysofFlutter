916b0b7a3b5ac7986175d888153b788c5f2109c5
Ayush Verma <ayush7607verma@gmail.com>
2022-08-25 14:26:47 +0530

    first commit done

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      home: Material(
+        child : Center(
+      child: Container (
+         child: Text("Lauda Welcome"),
+      ),
+        ), 
+    ),
+     );
+    // ui ka kaam
+  }
+}
